4de86e3c4c3d1327936eb159ae6db1bec08bd158
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-11-20 20:00:26 -0300

    🚸 ux: improve login form messages

--- src/pages/auth/Login/schema.ts
+++ src/pages/auth/Login/schema.ts
@@ -2,8 +2,8 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 export const LoginFormSchema = z.object({
-  email: z.string().email("E-mail inválido"),
-  password: z.string(),
+  email: z.string().email("E-mail inválido."),
+  password: z.string().min(1, "Não pode ser vazio."),
 });
 
 export const LoginFormObject = {
