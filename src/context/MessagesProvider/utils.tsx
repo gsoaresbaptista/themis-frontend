5f1b541a7ca3af0a5c60c8853ee80f27e734729c
Gabriel Soares Baptista <gsoaresbaptista@gmail.com>
2023-12-15 10:33:26 -0300

    🐛 fix: remove seconds from fake messages

--- src/context/MessagesProvider/utils.tsx
+++ src/context/MessagesProvider/utils.tsx
@@ -19,6 +19,5 @@ export function getMessagesLocalStorage() {
 export function formatDate(date: Date) {
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}`;
+  return `${hours}:${minutes}`;
 }
